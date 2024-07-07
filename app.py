891c5ffcdac45aa045ecbd3f7f5c36609c1508a8
SjoerdCor <sjoerdcornelissen@gmail.com>
2024-07-07 15:28:13 +0200

    Add flask session for thread-safety

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, session
 from dotenv import load_dotenv
 from tweevoortwaalf.woordrader import WoordRader
 
@@ -12,29 +12,32 @@ app.secret_key = os.getenv('FLASK_SECRET_KEY')
 
 @app.route('/')
 def index():
-    global twaalfletterwoord
-    twaalfletterwoord = WoordRader()
-    return render_template('index.html', state=twaalfletterwoord.state)
+    if 'game_state' not in session:
+        twaalfletterwoord = WoordRader()
+        session['game_state'] = twaalfletterwoord.state
+        session['answer'] = twaalfletterwoord.answer
+    return render_template('index.html', state=session['game_state'])
 
 @app.route('/buy_letter', methods=['POST'])
 def buy_letter():
     data = request.json
-    quizposition = int(data.get('quizposition'))
-    twaalfletterwoord.state[quizposition]['bought'] = True
-    return jsonify(twaalfletterwoord.state)
+    quizposition = data.get('quizposition')
+    session['game_state'][quizposition]['bought'] = True
+    return jsonify(session['game_state'])
 
 @app.route('/guess', methods=['POST'])
 def guess():
     data = request.json
     guess = data.get('guess')
-    result = "Correct" if guess.lower().replace("ij", "\u0133") == twaalfletterwoord.answer.lower() else "Incorrect"
-    result += f"! The correct answer is {twaalfletterwoord.answer.lower()!r}"
+    answer = session['answer'].lower()
+    result = "Correct" if guess.lower().replace("ij", "\u0133") == answer else "Incorrect"
+    result += f"! The correct answer is {answer!r}"
     return jsonify({"result": result})
 
 @app.route('/play_again', methods=['POST'])
 def play_again():
-    global twaalfletterwoord
-    twaalfletterwoord = WoordRader()
+    session.pop('game_state', None)
+    session.pop('answer', None)
     return jsonify({"message": "Game reset."})
 
 if __name__ == '__main__':
