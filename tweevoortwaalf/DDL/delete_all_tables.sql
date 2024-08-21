4af769c300a4f155f91afbaecbee84c5ed6e47a3
SjoerdCor <sjoerdcornelissen@gmail.com>
2024-08-21 19:01:29 +0200

    add deletion script

---
+++ tweevoortwaalf/DDL/delete_all_tables.sql
@@ -0,0 +1,10 @@
+DROP TABLE woordrader.games CASCADE;
+DROP TABLE woordrader.shownletters;
+DROP TABLE woordrader.boughtletters;
+DROP TABLE woordrader.guesses;
+
+DROP TABLE paardensprong.games CASCADE;
+DROP TABLE paardensprong.guesses;
+
+DROP TABLE taartpuzzel.games CASCADE;
+DROP TABLE taartpuzzel.guesses;
