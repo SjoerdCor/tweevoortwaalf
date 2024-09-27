4ec4aa08e8aa43d068e5089072515f31aa9be3d0
SjoerdCor <sjoerdcornelissen@gmail.com>
2024-09-27 14:16:15 +0200

    Move letter buy to woordrader.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,4 @@
-let resultDiv, newGameForm, submitGuessForm, timerElement, countdown, lettersBoughtCount;
+let resultDiv, newGameForm, submitGuessForm, timerElement, countdown;
 function disableForm(form) {
     Array.from(form.elements).forEach(element => {
         element.disabled = true;
@@ -197,33 +197,3 @@ document.addEventListener('DOMContentLoaded', () => {
     handleSubmit()
 
 });
-
-function buyLetterEvent(event) {
-    const pos = event.currentTarget.getAttribute('data-pos');
-    buyLetter(pos);
-
-    lettersBoughtCount++;
-    document.getElementById('letters-bought').textContent = lettersBoughtCount;
-
-    disableClickabilityCell(pos);
-}
-
-function buyLetter(quizposition) {
-    fetch('/buy_letter', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ quizposition }),
-    })
-        .then(response => response.json())
-        .then(data => {
-            const cellUpperRow = document.getElementById(`upperrow-${quizposition}`);
-            const cellLowerRow = document.getElementById(`lowerrow-${data[quizposition].answer_position}`);
-            const letterData = data[quizposition];
-
-            cellUpperRow.textContent = "";
-            const letter = capitalizeLetterExceptI(letterData.true_letter)
-            cellLowerRow.textContent = letterData.correct ? letter : "?";
-        });
-}
--- static/js/woordrader.js
+++ static/js/woordrader.js
@@ -1,3 +1,5 @@
+let lettersBoughtCount;
+
 document.addEventListener('DOMContentLoaded', () => {
     setupGameForm({
         puzzleLettersQuery: '#top-row .cell',
@@ -6,3 +8,33 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 }
 );
+
+function buyLetterEvent(event) {
+    const pos = event.currentTarget.getAttribute('data-pos');
+    buyLetter(pos);
+
+    lettersBoughtCount++;
+    document.getElementById('letters-bought').textContent = lettersBoughtCount;
+
+    disableClickabilityCell(pos);
+}
+
+function buyLetter(quizposition) {
+    fetch('/buy_letter', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ quizposition }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            const cellUpperRow = document.getElementById(`upperrow-${quizposition}`);
+            const cellLowerRow = document.getElementById(`lowerrow-${data[quizposition].answer_position}`);
+            const letterData = data[quizposition];
+
+            cellUpperRow.textContent = "";
+            const letter = capitalizeLetterExceptI(letterData.true_letter)
+            cellLowerRow.textContent = letterData.correct ? letter : "?";
+        });
+}
